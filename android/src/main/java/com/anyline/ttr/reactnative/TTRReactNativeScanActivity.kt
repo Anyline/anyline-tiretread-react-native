0038f7722f3209e8f05f79a03dadfb79aa90397f
David Dengg <david.dengg@anyline.com>
2025-08-28 12:54:08 +0200

    fix: removes volume button trigger on android

--- android/src/main/java/com/anyline/ttr/reactnative/TTRReactNativeScanActivity.kt
+++ android/src/main/java/com/anyline/ttr/reactnative/TTRReactNativeScanActivity.kt
@@ -1,15 +1,11 @@
 package com.anyline.ttr.reactnative
 
 import android.os.Bundle
-import android.view.KeyEvent
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.anyline.tiretread.sdk.config.TireTreadConfig
 import io.anyline.tiretread.sdk.scanner.AnylineInternalFeature
-import io.anyline.tiretread.sdk.scanner.DistanceStatus
 import io.anyline.tiretread.sdk.scanner.ScanEvent
 import io.anyline.tiretread.sdk.scanner.TireTreadScanView
-import io.anyline.tiretread.sdk.scanner.TireTreadScanner
 import kotlinx.serialization.json.Json
 
 class TTRReactNativeScanActivity : AppCompatActivity() {
@@ -76,28 +72,4 @@ class TTRReactNativeScanActivity : AppCompatActivity() {
     finish()
   }
 
-  override fun dispatchKeyEvent(event: KeyEvent): Boolean {
-    return when (event.keyCode) {
-      KeyEvent.KEYCODE_VOLUME_UP, KeyEvent.KEYCODE_VOLUME_DOWN -> {
-        if (event.action == KeyEvent.ACTION_UP) return true
-
-        if (TireTreadScanner.instance.isScanning) {
-          TireTreadScanner.instance.stopScanning()
-        } else {
-          if (TireTreadScanner.instance.captureDistanceStatus == DistanceStatus.OK)
-            TireTreadScanner.instance.startScanning()
-          else {
-            Toast.makeText(
-              this,
-              "Move the phone to the correct position before starting.",
-              Toast.LENGTH_SHORT
-            ).show()
-          }
-        }
-        true
-      }
-
-      else -> super.dispatchKeyEvent(event)
-    }
-  }
 }
